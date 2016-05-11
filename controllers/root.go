bebf8b2bc7e0d9b4c35fde8b7c7723ca8f513506
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2016-05-11 11:16:52 -0400

    Woops

--- controllers/root.go
+++ controllers/root.go
@@ -8,7 +8,7 @@ import (
 	"github.com/flosch/pongo2"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
-	"gopkg.in/mgo.v2/bson"
+	_ "gopkg.in/mgo.v2/bson"
 
 	// Project
 	"github.com/danielbarbarito/go-web-template/models"
